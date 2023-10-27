55d079977a374710093e689125eef2ac51980a58
DARVAR2K <darvar2k@gmail.com>
2023-10-26 21:35:24 -0600

    menu principal

---
+++ src/main/java/cr/ac/una/pac/man/controller/PlayerNameController.java
@@ -0,0 +1,63 @@
+package cr.ac.una.pac.man.controller;
+
+import cr.ac.una.pac.man.util.Mensaje;
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author dario
+ */
+public class PlayerNameController extends Controller implements Initializable {
+
+    @FXML
+    private TextField txt_PlayerName;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void initialize() {}
+    
+            private void addPalyer() {
+        try {
+            String nombre = txt_PlayerName.getText();
+
+            String contenido;
+
+            File scoress = new File(".\\src\\main\\resources\\cr\\ac\\una\\cubeproject\\files\\player\\player.txt");
+
+            FileWriter scores = new FileWriter(scoress, true);
+            scores.write(nombre + "***");
+            scores.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void onAction_savePlayer(ActionEvent event) {
+        
+        if(txt_PlayerName.getText().length() > 0){
+            addPalyer();
+        }else{
+            new Mensaje().show(Alert.AlertType.ERROR, "Nombre Jugador", "Ingresa el nombre del jugador.");
+        }
+        
+    }
+    
+}
--- src/main/java/cr/ac/una/pac/man/controller/WelcomeView.java
+++ src/main/java/cr/ac/una/pac/man/controller/WelcomeView.java
@@ -1,7 +1,12 @@
 package cr.ac.una.pac.man.controller;
 
+import cr.ac.una.pac.man.util.FlowController;
+import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -9,13 +14,50 @@ import javafx.fxml.Initializable;
  */
 public class WelcomeView extends Controller implements Initializable {
 
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-   
-    }    
+
+    }
 
     @Override
-    public void initialize() {}
-    
+    public void initialize() {
+    }
+
+    private boolean archivoNoVacio() {
+        File player = new File(".\\src\\main\\resources\\cr\\ac\\una\\cubeproject\\files\\player\\player.txt");
+        return player.exists() && player.length() > 0;
+    }
+
+    @FXML
+    private void onAction_start(ActionEvent event) {
+
+        if (archivoNoVacio()) {
+             FlowController.getInstance().goViewInWindow("GameView");
+        getStage().close();
+            
+        } else {
+             FlowController.getInstance().goViewInWindow("PlayerNameView");
+        getStage().close();
+        }
+
+       
+    }
+
+    @FXML
+    private void onAction_player(ActionEvent event) {
+        FlowController.getInstance().goViewInWindow("PalyerView");
+        getStage().close();
+    }
+
+    @FXML
+    private void onAction_settings(ActionEvent event) {
+        FlowController.getInstance().goViewInWindow("SettingView");
+        getStage().close();
+    }
+
+    @FXML
+    private void onAction_exit(ActionEvent event) {
+        getStage().close();
+    }
+
 }
